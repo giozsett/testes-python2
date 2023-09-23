b3b95070c077bb4362f16a2ced707bcc9df80c3e
Giovana Z Corsetti <giovana.corsetti@outlook.com>
2023-09-23 11:24:13 -0300

    deixando mais bonitin

--- tabuada.py
+++ tabuada.py
@@ -2,7 +2,14 @@
 # Escreva um código para imprimir a tabuada do 2 usando o laço for.
 # Refatore o código para solicitar ao usuário qual tabuada deve ser impressa.
 
+print("")
+print("CÁLCULO DA TABUADA")
+print("")
+print("Feito para calcular os múltiplos de 0 a 10 de um número.")
+print("")
+print("")
 
+print("Tabuada do número 2:")
 # PARTE 1 (tabuada do 2)
 for numero2 in range(0, 11):
     print("2 x", numero2, "=", numero2 * 2)
