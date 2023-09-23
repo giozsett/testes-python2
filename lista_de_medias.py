8acd45fc98c941dcbcc44d4ec37aa6d315b9b595
Giovana Z Corsetti <giovana.corsetti@outlook.com>
2023-09-23 15:02:51 -0300

    corringido list medis

--- lista_de_medias.py
+++ lista_de_medias.py
@@ -2,29 +2,23 @@
 # Programa deve solicitar o nome dos alunos e suas notas de 4 provas.
 # Ao final deve imprimir o nome dos alunos e suas médias.
 
-
-lista_alunos = []
-lista_alunos.append(input("Informe o nome do aluno: "))
-primeira_nota = float(input("Informe a nota da 1ª prova: "))
-segunda_nota = float(input("Informe a nota da 2ª prova: "))
-terceira_nota = float(input("Informe a nota da 3ª prova: "))
-quarta_nota = float(input("Informe a nota da 4ª prova: "))
+quantidade_alunos = 1
+nome_alunos = []
 media_alunos = []
-media_alunos.append((primeira_nota + segunda_nota + terceira_nota + quarta_nota) / 4)
-incluir_aluno = input("Deseja incluir mais algum aluno? (s/n) ")
+incluir_aluno = "s"
 
 
 while incluir_aluno == "s":
-    lista_alunos = []
-    lista_alunos.append(input("Informe o nome do aluno: "))
+    quantidade_alunos = quantidade_alunos + 1 
+    nome_alunos.append(input("Informe o nome do aluno: "))
     primeira_nota = float(input("Informe a nota da 1ª prova: "))
     segunda_nota = float(input("Informe a nota da 2ª prova: "))
     terceira_nota = float(input("Informe a nota da 3ª prova: "))
     quarta_nota = float(input("Informe a nota da 4ª prova: "))
-    media_alunos = []
     media_alunos.append((primeira_nota + segunda_nota + terceira_nota + quarta_nota) / 4)
     incluir_aluno = input("Deseja incluir mais algum aluno? (s/n) ")
 
 if incluir_aluno == "n":
-    print(lista_alunos)
-    print(media_alunos)
+    for quantidade_alunos in range(quantidade_alunos):
+        print(nome_alunos)
+        print(media_alunos)
